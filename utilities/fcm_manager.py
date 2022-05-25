38541277bbce3b1a0ec97bf08ff259237afb08d4
Georgio Sunil <georgio.p@wm-int.com>
2022-05-25 11:02:49 +0400

    feat: image upload for thimbnails on notifications.

--- utilities/fcm_manager.py
+++ utilities/fcm_manager.py
@@ -5,16 +5,20 @@ from firebase_admin import credentials, messaging, firestore
 cred = credentials.Certificate("utilities/credentials/wm-notifications-firebase-adminsdk.json")
 firebase_admin.initialize_app(cred)
 
-def sendPush(title, msg, registration_token, dataObject=None):
+def sendPush(title, msg, image, registration_token, dataObject=None):
     
     message = messaging.MulticastMessage(
         notification=messaging.Notification(
             title=title,
-            body=msg
+            body=msg,
+            image = image
         ),
         data=dataObject,
         tokens=registration_token,
     )
 
     response = messaging.send_multicast(message)
+    # print(response.failure_count)
+    # print(response.responses[0].exception)
+    # print(response.success_count)
     print('Successfully sent message:', response)
